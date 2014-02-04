22d633bcf23f32621e9360df2e235a1c8837ac58
ako80218 <andrew@andrewotto.com>
2014-02-04 09:42:25 -0700

    part 1 done

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 
-var FoodItem = function(name, calories){
+var FoodItem = function(name, calories, isVegan, isGlutenFree, isCitrusFree){
 this.name = name;
 this.calories=calories;
+this.vegan = isVegan || false;
+this.glutenFree = isGlutenFree || false;
+this.citrusFree = isCitrusFree || false;
 }
+
 FoodItem.prototype.toString = function(){
-	console.log ("The {0} is {1}  calories.".supplant([this.name, this.calories]));
+	console.log ("The {0} is {1}  calories.{2}{3}{4}".supplant([this.name, this.calories,
+		this.vegan ? "  This item is vegan." : "",
+		this.glutenFree ? "  This item is gluten free." : "", 
+		this.citrusFree ? "  This item is citrus free." : "" ]));
 };
-
-var taco = new FoodItem('taco', 200);
-
-
+var taco = new FoodItem('taco', 200, true, false, true);
+var salad = new FoodItem('salad', 150, true, true, true);
+var steak = new FoodItem('steak', 300, false, true, true);
 taco.toString();
+salad.toString();
+steak.toString();
 
-console.log(FoodItem.length);
+
 
 
